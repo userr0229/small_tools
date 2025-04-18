6eefee179f2808d2ddba77442ddd862699422165
Your Name <you@example.com>
2025-04-18 13:13:54 +0800

    Using a Timer to reset the counter in media_control.py

--- media_control.py
+++ media_control.py
@@ -1,47 +1,51 @@
-"""
-A simple media control script that uses Ctrl + Space to control music playback.
-
-Controls:
-- Press Ctrl + Space 1 time: Play/Pause
-- Press Ctrl + Space 2 times: Previous track
-
-Requirements:
-- keyboard module (pip install keyboard)
-"""
-
-import keyboard
+# Version: 2.0 Using Timer to reset counter
 import pyautogui
-import time
+import keyboard
+from threading import Timer
 
-# Variables to track key press time and count
-last_press_time = 0
-press_count = 0
-def control_music():
-    global last_press_time, press_count
-    
-    current_time = time.time()
-    
-    # If interval between two key presses is less than 0.5 seconds, increment counter
-    if current_time - last_press_time < 0.5:
-        press_count += 1
-    else:
-        press_count = 1  # Reset counter
+# Define functions for each action
+def pause_music():
+    print("Pause music")
+    keyboard.send("play/pause media")
 
-    last_press_time = current_time
+def previous_song():
+    print("Previous track")
+    pyautogui.press('prevtrack')
 
-    if press_count == 1:
-        # Single press: Pause/Play
-        print("Ctrl + Space: Pause/Play music")
-        # Send Play/Pause control command
-        keyboard.send("play/pause media")
-    elif press_count == 2:
-        # Double press: Previous track
-        print("Ctrl + Space: Switch to previous track")
-        # Send previous track shortcut
-        pyautogui.press('prevtrack')
-        press_count = 0  # Reset counter
+def next_song():
+    print("Next track")
+    pyautogui.press('nexttrack')
 
-# Listen for Ctrl + Space
-keyboard.add_hotkey("ctrl+space", control_music)
+# Global variables: counter and timer
+press_counter = 0
+reset_timer = None
 
+# Reset counter and trigger corresponding function based on press count
+def reset_counter():
+    global press_counter, reset_timer
+    if press_counter == 1:
+        pause_music()
+    elif press_counter == 2:
+        previous_song()
+    elif press_counter == 3:
+        next_song()
+    # Reset state
+    press_counter = 0
+    reset_timer = None
+
+# Hotkey callback function
+def hotkey_pressed():
+    global press_counter, reset_timer
+    press_counter += 1
+    # Cancel existing timer if any
+    if reset_timer is not None:
+        reset_timer.cancel()
+    # Set a short delay (e.g. 0.5 seconds), execute corresponding function if no new keypress
+    reset_timer = Timer(0.5, reset_counter)
+    reset_timer.start()
+
+# Register Ctrl+Space hotkey
+keyboard.add_hotkey('ctrl+space', hotkey_pressed)
+
+print("Music control started, press Ctrl+Space to control")
 keyboard.wait()  # Keep program running
